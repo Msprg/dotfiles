db8dcd9d8c374fc0149c684e807945da2ea43394
Msprg <18015488+Msprg@users.noreply.github.com>
2023-06-30 03:57:18 +0200

    Initial heavy modification, mixed together stuff from multiple repos

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,9 +8,10 @@ function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
+		--exclude "*.sh" \
+		--exclude "*.md" \
+		--exclude "*.disabled" \
+		--exclude "*.txt" \
 		-avh --no-perms . ~;
 	source ~/.bash_profile;
 }
